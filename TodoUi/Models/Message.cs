9f38501736c67c46725aeda234bcaf2cc936fa22
大溝桂 <kay.akubicharm.dev@gmail.com>
2024-04-04 04:28:50 +0900

    update Message class

--- TodoUi/Models/Message.cs
+++ TodoUi/Models/Message.cs
@@ -4,5 +4,5 @@ public class Message
     public string? User { get; set; }
     public string? Text { get; set; }
 
-    public string? System { get; set; }
+    public string? Role { get; set; }
 }
